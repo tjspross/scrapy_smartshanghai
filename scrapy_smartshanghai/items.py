7fa3c10e4fa0e6e913a38bf571297060edff359e
tj <tj@italki.com>
2017-12-30 11:18:08 -0700

    making a lot of changes to scraper. Scraper runs more asynchronously now. Consolidating output files into listings.jl

--- scrapy_smartshanghai/items.py
+++ scrapy_smartshanghai/items.py
@@ -7,3 +7,45 @@
 
 import scrapy
 
+class ScrapySmartShanghaiItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+
+    posted_type = scrapy.Field()
+    listing_id = scrapy.Field()
+    view_count = scrapy.Field()
+    scrape_time = scrapy.Field()
+    latitude = scrapy.Field()
+    longitude = scrapy.Field()
+    floor = scrapy.Field()
+    metro = scrapy.Field()
+    size = scrapy.Field()
+    author = scrapy.Field()
+    area = scrapy.Field()
+    compound = scrapy.Field()
+    publish_time = scrapy.Field()
+    description = scrapy.Field()
+    furnished = scrapy.Field()
+    rooms = scrapy.Field()
+    price = scrapy.Field()
+    title = scrapy.Field()
+
+    # Utilities
+    washing_machine = scrapy.Field()
+    parking = scrapy.Field()
+    tv = scrapy.Field()
+    central_aircon = scrapy.Field()
+    security = scrapy.Field()
+    balcony = scrapy.Field()
+    elevator = scrapy.Field()
+    playground = scrapy.Field()
+    oven = scrapy.Field()
+    dryer = scrapy.Field()
+    dvd_player = scrapy.Field()
+    health_club = scrapy.Field()
+    pool = scrapy.Field()
+    outdoor_space = scrapy.Field()
+    air_filter = scrapy.Field()
+    water_filter = scrapy.Field()
+    floor_heating = scrapy.Field()
+    wall_heating = scrapy.Field()
--- scrapy_smartshanghai/pipelines.py
+++ scrapy_smartshanghai/pipelines.py
@@ -4,8 +4,42 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import os
+from datetime import datetime
+
+import pandas as pd
+from scrapy.exceptions import DropItem
 
 
-class ScrapySmartshanghaiPipeline(object):
+def myconverter(o):
+    if isinstance(o, datetime):
+        return o.__str__()
+
+
+class ScrapySmartShanghaiPipeline(object):
+    def __init__(self):
+        self.file_path = os.path.dirname(os.path.dirname(__file__)) + "/output/listings.jl"
+
+        df = pd.read_json(self.file_path, lines=True)
+        if df.empty:
+            self.ids_seen=set()
+        else:
+            self.ids_seen = set(df.listing_id)
+
+    def open_spider(self, spider):
+        self.file = open(self.file_path, 'a')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
-        return item
+        if item['listing_id'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        elif item['author'] is None:
+            raise DropItem("Null listing found: %s" % item)
+        else:
+            self.ids_seen.add(item['listing_id'])
+            line = json.dumps(dict(item), default=myconverter) + "\n"
+            self.file.write(line)
+            return item
